b569462f5457d9d3c3b8d5a5a58a89df8db4dc00
Alena Kashayed <alena_kashayed@epam.com>
2017-12-01 13:16:56 +0300

    task 5 fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
-import { StaticRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
@@ -21,7 +21,7 @@ const store = createStore(reducer, composeEnhancers(
   applyMiddleware(thunk),
 ));
 
-ReactDOM.hydrate((
+ReactDOM.render((
   <Provider store={store}>
     <Router>
       <App />
